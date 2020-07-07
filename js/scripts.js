5007c3f7147c9c0a504e350a924548af0caac4e2
Luke Hoang <lukehoang195@gmail.com>
2020-07-07 16:51:50 +0700

    finish functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,17 @@ let amountInput = document.getElementById('amount');
 let convertButton = document.getElementById('convertButton');
 let fromCurrency = document.getElementById("fromCurrencyList");
 let toCurrency = document.getElementById("toCurrencyList");
+let currencyList = {};
 
-const exchangeRate = {
-    USD: 23208,
-    AUD: 15964,
-    CAD: 16915,
-    CHF: 24399,
-    EUR: 25950,
-    GBP: 28600,
-    JPY: 213.45,
-    SGD: 16455,
-    VND: 1
+
+async function callApi() {
+    let url = "http://data.fixer.io/api/latest?access_key=0f2611ea1f1ff407b0040f536eebae82"
+    const response = await fetch(url);
+    let result = await fetch(url);
+    let data = await result.json();
+    currencyList = data.rates
 }
+callApi();
 
 function formatCurrency(type, value) {
     const formatter = new Intl.NumberFormat(type, {
@@ -24,14 +23,17 @@ function formatCurrency(type, value) {
 }
 
 function exchange() {
+    console.log(currencyList["VND"])
+
     let amount = amountInput.value;
     let fcurrency = fromCurrency.value;
     let tcurrency = toCurrency.value;
-    //to vnd
-    amountVnd = amount * exchangeRate[fcurrency];
-    //out not vnd
-    let convertedAmount = Math.round(((amountVnd / exchangeRate[tcurrency]) + Number.EPSILON) * 100) / 100;
+
+    let amountEur = Math.round(((amount / currencyList[fcurrency]) + Number.EPSILON) * 100) / 100;
+    let convertedAmount = amountEur * currencyList[tcurrency];
+
     let formatAmount = formatCurrency(tcurrency, convertedAmount)
     document.getElementById("result").innerHTML = `your money is ${formatAmount}`;
+
 }
 convertButton.addEventListener("click", exchange)
